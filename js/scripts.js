fbea88782e7e530a07d5efb768c87c9d418df98e
juandavidpelaez <juandavid.pelaez2@gmail.com>
2019-03-26 18:30:28 -0500

    dynamic modal added

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 
 $(document).ready(function(){
 
+  var dict = {
+    "sport":{"label":"label-info","class":".sport","id":"#sport","ref":"[href='#sport']"},
+    "college":{"label":"label-warning","class":".college","id":"#college","ref":"[href='#college']"},
+    "fit":{"label":"label-success","class":".fit","id":"#fit","ref":"[href='#fit']"},
+    "hipster":{"label":"label-danger","class":".hipster","id":"#hipster","ref":"[href='#hipster']"}
+  };
   //sidebar pills filter
   $(".nav-pills li a").click(function(){
-      var clase = $(this).attr('data-filter');
-      $(".filter").not('.'+clase).hide('3000');
-      $('.filter').filter('.'+clase).show('3000');        
+      var filtro = $(this).attr('data-filter');
+      $(".filter").not('.'+filtro).hide('3000');
+      $('.filter').filter('.'+filtro).show('3000');        
     });
 
   //partial class match filter through the form-control, search button listener (experimental, for fun)
@@ -15,11 +21,68 @@ $(document).ready(function(){
       $(this).toggle($(this).attr("class").toLowerCase().indexOf(value) > -1 )
     });
   });
+  
+  //setting badge text for fun
+  $("[href='#all'] .badge").text($(".product-row .filter").length);
+  $.each(dict, function(i,item){
+	  $(dict[i].ref+" .badge").text($(".product-row "+dict[i].class).length);
+	});
+  // $("[href='#hipster'] .badge").text($(".product-row .hipster").length);
+  // $("[href='#fit'] .badge").text($(".product-row .fit").length);
+  // $("[href='#college'] .badge").text($(".product-row .college").length);
+  // $("[href='#sport'] .badge").text($(".product-row .sport").length);
 
-  //setting text for fun, just in the first badge text
-  $(".nav-pills li:first-child a span").text($(".product-row .filter").length);
-  $(".nav-pills li:nth-child(2) a span").text($(".product-row .hipster").length);
-  $(".nav-pills li:nth-child(3) a span").text($(".product-row .college").length);
-  $(".nav-pills li:nth-child(4) a span").text($(".fit").length);
-  $(".nav-pills li:last-child a span").text($(".sport").length);
+  $(".modal-button").click(function(){
+    // clasparent = getClothingClassParent(this);
+    //clas = $(this).parent().attr("class")
+    $("#myModal").on('show.bs.modal', function (e) {
+      
+      var button = $(e.relatedTarget);
+      var parent = button.parent();
+      var title = parent.find("h3").text();
+      var img = parent.find("img").attr("src")
+      var clas = getClothingClassParent(button);
+      var description = parent.find("p").text();
+
+
+      var modal = $(this);
+
+      modal.find('.modal-title').text(title);
+      modal.find('img').attr("src",img);
+      modal.find('#description').text(description);
+
+      $.each(clas, function(i,item){
+        modal.find("."+dict[item].label).text(item);
+      });
+    });
+    
+    $("#myModal").on("hidden.bs.modal", function(e){
+      var modal = $(this);
+      modal.find('.modal-title').empty();
+      modal.find('img').attr("src","");
+      modal.find('#description').empty();
+      modal.find(".label").empty();
+    });
+
+  });
+
+  function getClothingClass(element){
+    var classlist = ["hipster", "college","fit","sport"];
+    var clases = element.attr("class");
+    clases = clases.split(" ");
+    clases = $.grep(classlist, function(e){
+      return $.inArray(e, clases) !== -1;
+    });
+    return clases;
+  };
+  function getClothingClassParent(element){
+    var classlist = ["hipster", "college","fit","sport"];
+    var clases = element.parent().attr("class");
+    clases = clases.split(" ");
+    clases = $.grep(classlist, function(e){
+      return $.inArray(e, clases) !== -1;
+    });
+    return clases;
+  }; 
+
 });
